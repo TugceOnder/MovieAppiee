6a7bd63bae1765d5f62158dd0deadbdae1502ccf
TugceOnder <tugceonder775@gmail.com>
2025-09-28 00:06:14 +0300

    Add project files.

---
+++ Infrastructure/MovieAppie.Persistance/Context/MovieContext.cs
@@ -0,0 +1,28 @@
+﻿using Azure;
+using Microsoft.EntityFrameworkCore;
+using MovieApi.Domain;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieAppie.Persistance.Context
+{
+    public class MovieContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(
+     "Server=localhost\\SQLEXPRESS;Initial Catalog=ApiMovieDb;Integrated Security=True;TrustServerCertificate=True");
+
+        }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Movie> Movies { get; set; }
+        public DbSet<Review> Reviews { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+        public DbSet<Cast> Casts { get; set; }
+
+    }
+}
